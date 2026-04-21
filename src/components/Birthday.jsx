7df0992d5633bb7670d79d19cfe9a0add9f334ac
Montgomery Mcdonald <halfbloodhero1027@gmail.com>
2026-04-20 17:39:59 -0700

    netlify build fix #1

--- src/components/Birthday.jsx
+++ src/components/Birthday.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
 
-import Form from './Form'
 import GetAllEvents from './GetAllEvents'
 
 const Birthday = () => {
 
     const [events, setEvents] = useState([{eventName: "Moxks-HRT", eventDate: new Date("8/17/2026"), id: 0}])
     
-    function handleFormInput(){
-        console.log("handling form input...")
-        setEvents([...events, {eventName : document.getElementById("event-name").value, eventDate : new Date(document.getElementById("event-date").value), id:events.length}])
-        console.log(events)
-    }
 
     // function getWeeklyEvents(){
 
